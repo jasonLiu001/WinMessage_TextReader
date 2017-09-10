63e0178cc9c2e7329529fc8dac465ff3601621e6
wangliu <wangliu@isoftstone.com>
2017-09-10 14:17:43 +0800

    updated.

--- WinMessage-TextReader/Program.cs
+++ WinMessage-TextReader/Program.cs
@@ -11,50 +11,15 @@ namespace WinMessage_TextReader
 {
     class Program
     {
-        /// <summary>
-        /// 文件路径
-        /// </summary>
-        public static string filePath = ConfigurationManager.AppSettings["filePath"].ToString();
-
-        /// <summary>
-        /// 计时器
-        /// </summary>
-        private static System.Timers.Timer aTimer = new System.Timers.Timer();
-
-        /// <summary>
-        /// 上次的计划文字
-        /// </summary>
-        private static string lastPlanText = string.Empty;
 
         static void Main(string[] args)
         {
-            //设置计时器
-            //SetTimer();
-            GetPlanText();
+            var captionTitle = GetWindowCaptionTitle();
+            Console.WriteLine(captionTitle);
             Console.ReadLine();
         }
 
-        /// <summary>
-        /// 获取计划字符串
-        /// </summary>
-        /// <returns></returns>
-        private static string GetPlanText()
-        {
-            var txt = string.Empty;
-            var titleText = GetWindowCaptionTitle();
-            var txtArray = titleText.Split(new string[] { "\r\n" }, StringSplitOptions.RemoveEmptyEntries);
 
-            for (var i = 0; i < txtArray.Length; i++)
-            {
-                var itemText = txtArray[i];
-                if (itemText.Contains("计划进行"))
-                {
-                    txt = itemText;
-                    break;
-                }
-            }
-            return txt;
-        }
 
         /// <summary>
         /// 获取窗口标题文字
@@ -79,43 +44,6 @@ namespace WinMessage_TextReader
             Win32.SendMessageW2(planWin, Constant.WM_GETTEXT, (uint)maxLength, buffer);
             string windowCaptionTitle = Marshal.PtrToStringUni(buffer);
             return windowCaptionTitle;
-        }
-
-
-        /// <summary>
-        /// 设置计时器
-        /// </summary>
-        private static void SetTimer()
-        {
-            aTimer.Elapsed += new ElapsedEventHandler(TimeEvent);
-            // 设置引发时间的时间间隔 此处设置为１秒（１０００毫秒）
-            aTimer.Interval = 10000;
-            aTimer.Enabled = true;
-        }
-
-        /// <summary>
-        /// 计时器定时触发事件
-        /// </summary>
-        private static void TimeEvent(object sender, ElapsedEventArgs e)
-        {
-            //首次
-            if (string.IsNullOrEmpty(lastPlanText))
-            {
-                var titleText = GetWindowCaptionTitle();
-                //保存文本
-                lastPlanText = titleText;
-                //保存到文件
-                Console.WriteLine(titleText);
-                return;
-            }
-
-            var newTitleText = GetWindowCaptionTitle();
-            if (newTitleText == lastPlanText) return; //计划未更新
-
-            //保存新计划文本
-            lastPlanText = newTitleText;
-            //保存到文件
-            Console.WriteLine(newTitleText);
         }
     }
 }
